96d28d4dc1aba69321329379c13c9eaf3945ae75
RomanovskiiArsenii <romanovskyway@gmail.com>
2024-02-27 06:33:34 +0400

    init

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const glowingText = (() => {
+    const textElement = document.getElementById('glowing_text');
+    let coloredText;
+    let index = 0;
+
+    const startGlowing = () => {
+        setInterval(() => {
+            const letters = textElement.textContent.split('');
+            if (letters[index] == ' ') {
+                index++;
+            }
+            letters[index] = `<span style="text-shadow: 0 0 .15em var(--shadowColor)">${letters[index]}</span>`;
+            letters[index] = `<span style="color: var(--glowColor)">${letters[index]}</span>`;
+            coloredText = letters.join('');
+            textElement.innerHTML = coloredText;
+            index = (index + 1) % letters.length;
+        }, 500);
+    };
+
+    return startGlowing;
+})();
+
+window.addEventListener('load', glowingText, false);
